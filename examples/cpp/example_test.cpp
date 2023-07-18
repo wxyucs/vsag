ba3011df0407c505d313a8550df172eddb39ba92
jinjiabao.jjb <jinjiabao.jjb@antgroup.com>
2023-07-18 14:27:41 +0800

    add unit test

--- examples/cpp/example_test.cpp
+++ examples/cpp/example_test.cpp
@@ -9,28 +9,27 @@ main() {
     int ef_construction = 200;  // Controls index search speed/build speed tradeoff
 
     // Initing index
-    hnswlib::InnerProductSpaceInt8 space(dim);
-    hnswlib::HierarchicalNSW* hnsw = new hnswlib::HierarchicalNSW(&space, max_elements, M, ef_construction);
+    vsag::HNSW hnsw(dim, max_elements, M, ef_construction);
 
     // Generate random data
     std::mt19937 rng;
     rng.seed(47);
     std::uniform_real_distribution<> distrib_real;
-    int8_t * data = new int8_t[dim * max_elements];
+    float * data = new float[dim * max_elements];
     for (int i = 0; i < dim * max_elements; i++) {
-        data[i] = distrib_real(rng) * 256;
+        data[i] = distrib_real(rng);
     }
 
     // Add data to index
     for (int i = 0; i < max_elements; i++) {
-        hnsw->addPoint(data + i * dim, i);
+        hnsw.addPoint(data + i * dim, i);
     }
 
     // Query the elements for themselves and measure recall
     float correct = 0;
     for (int i = 0; i < max_elements; i++) {
         std::priority_queue<std::pair<float, hnswlib::labeltype>> result =
-            hnsw->searchKnn(data + i * dim, 1);
+            hnsw.searchTopK(data + i * dim, 1);
         hnswlib::labeltype label = result.top().second;
         if (label == i)
             correct++;
