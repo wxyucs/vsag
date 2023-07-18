e9b4f102f35c63baa6104462144a8b9085110fcb
jinjiabao.jjb <jinjiabao.jjb@antgroup.com>
2023-07-18 14:37:47 +0800

    add unit test for int8

--- tests/test_hnsw.cpp
+++ tests/test_hnsw.cpp
@@ -49,3 +49,41 @@ TEST_CASE("Float Recall", "[hnsw]") {
 
     REQUIRE(recall == 1);
 }
+
+TEST_CASE("Int8 Recall", "[hnsw]") {
+
+    int dim = 256;
+    int max_elements = 10000;
+    int M = 64;
+    int ef_construction = 200;
+    // Initing index
+    vsag::HNSW hnsw(dim, max_elements, M, ef_construction);
+
+    // Generate random data
+    std::mt19937 rng;
+    rng.seed(47);
+    std::uniform_real_distribution<> distrib_real;
+    int8_t * data = new int8_t[dim * max_elements];
+    for (int i = 0; i < dim * max_elements; i++) {
+        data[i] = distrib_real(rng) * 256;
+    }
+
+    // Add data to index
+    for (int i = 0; i < max_elements; i++) {
+        hnsw.addPoint(data + i * dim, i);
+    }
+
+    // Query the elements for themselves and measure recall
+    float correct = 0;
+    for (int i = 0; i < max_elements; i++) {
+        std::priority_queue<std::pair<float, hnswlib::labeltype>> result =
+            hnsw.searchTopK(data + i * dim, 1);
+        hnswlib::labeltype label = result.top().second;
+        if (label == i)
+            correct++;
+    }
+    float recall = correct / max_elements;
+
+    REQUIRE(recall == 1);
+}
+
