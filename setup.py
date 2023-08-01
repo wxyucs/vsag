900f60a97c53bc3a5d5bafa1243e5817cd9d32a4
jinjiabao.jjb <jinjiabao.jjb@antgroup.com>
2023-08-01 17:20:04 +0800

    change python directory name

--- setup.py
+++ setup.py
@@ -1,9 +1,24 @@
 from setuptools import setup, Extension
 import glob
-
+import platform
 
 source_files = glob.glob('src/*.cpp')
-source_files.append('python/binding.cpp')
+source_files.append('python_bindings/binding.cpp')
+
+compile_flags = [
+    '-std=c++17',
+    '-g',
+    '-Ofast'
+]
+if platform.processor() == "x86_64":
+    compile_flags.extend([
+        '-mavx512f',
+        '-mavx512dq',
+        '-mavx512bw',
+        '-mavx512vl',
+        '-mavx',
+        '-msse',
+    ])
 
 ext_modules = [
     Extension(
@@ -17,17 +32,7 @@ ext_modules = [
             'extern/pybind11/include',
         ],
         language='c++',
-        extra_compile_args=[
-            '-std=c++17',
-            '-g',
-            '-Ofast',
-            '-mavx512f',
-            '-mavx512dq',
-            '-mavx512bw',
-            '-mavx512vl',
-            '-mavx',
-            '-msse',
-        ],
+        extra_compile_args=compile_flags,
         extra_link_args=['-lgomp'],
     )
 ]
