3121c1b2116935414e425d3764100145fe3425a7
jinjiabao.jjb <jinjiabao.jjb@antgroup.com>
2023-08-01 11:10:48 +0800

    implement kmeans

--- python/binding.cpp
+++ python/binding.cpp
@@ -5,14 +5,26 @@
 
 #include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
+#include "vsag/vsag.h"
+
+namespace py = pybind11;
+
 
 int add(int a, int b) {
     return a + b;
 }
+py::array_t<double> kmeans(py::array_t<float, py::array::c_style | py::array::forcecast>& datas, int clusters, const std::string& dis_type) {
+    auto data_shape = datas.shape();
+    py::ssize_t py_clusters(clusters);
+    auto data_size = data_shape[0];
+    auto dimension = data_shape[1];
+    auto centroids = py::array_t<float>(py::array::ShapeContainer{py_clusters, dimension});
+    vsag::kmeans_clustering(dimension, data_size, clusters, datas.data(), centroids.mutable_data(), dis_type);
+    return centroids;
+}
 
 
-
-
-PYBIND11_MODULE(pyVsag, m) {
+PYBIND11_MODULE(pyvsag, m) {
     m.def("add", &add, "A function which adds two numbers");
+    m.def("kmeans", &kmeans, "Kmeans");
 }
