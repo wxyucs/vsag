28c5a2d5b80fcd731c08b0b6181134c394fc85eb
jinjiabao.jjb <jinjiabao.jjb@antgroup.com>
2023-08-09 10:48:25 +0800

    redirect result output directory

--- benchs/benchmark/bench_hnsw.py
+++ benchs/benchmark/bench_hnsw.py
@@ -12,7 +12,7 @@ def measure_all(dataset, X_train, vector_count, ef_construction, M, ef_values, n
 
     X_test = np.array(dataset['test'])
     results = np.array(dataset['neighbors'])
-    print("\nRunning measure_all...")
+    logging.info("\nRunning measure_all...")
 
     hnsw = vsag.Index(X_train.shape[1], X_train.shape[0], "l2", "float32", M, ef_construction, ef_values[0])
 
@@ -21,7 +21,7 @@ def measure_all(dataset, X_train, vector_count, ef_construction, M, ef_values, n
 
 
     for ef_runtime in ef_values:
-        print("\nRunning queries with ef_runtime =", ef_runtime, "...")
+        logging.info("\nRunning queries with ef_runtime =", ef_runtime, "...")
         hnsw.setEfRuntime(ef_runtime)
         correct = 0
         bf_total_time = 0
@@ -52,35 +52,35 @@ def measure_all(dataset, X_train, vector_count, ef_construction, M, ef_values, n
             # flat_result_ids = []
             # for i, doc in enumerate(flat_result.docs):
             #     flat_result_ids.append(doc.doc_id)
-            # print(ground_truth, result)
+            # logging.info(ground_truth, result)
             correct += len(np.intersect1d(result, ground_truth))
 
         # Measure recall
         recall = float(correct)/(k*num_queries)
-        print("Average recall is:", recall)
+        logging.info("Average recall is:", recall)
 
-        # print("BF query per seconds: ", num_queries/bf_total_time)
-        # print("BF average lantency per query: ", bf_total_time/num_queries, "seconds")
-        # print("BF min lantency for query: ", bf_min_latency, "seconds")
-        # print("BF max lantency for query: ", bf_max_latency, "seconds")
+        # logging.info("BF query per seconds: ", num_queries/bf_total_time)
+        # logging.info("BF average lantency per query: ", bf_total_time/num_queries, "seconds")
+        # logging.info("BF min lantency for query: ", bf_min_latency, "seconds")
+        # logging.info("BF max lantency for query: ", bf_max_latency, "seconds")
 
-        print("HNSW query per seconds: ", num_queries/hnsw_total_time)
-        print("HNSW average lantency per query: ", hnsw_total_time/num_queries, "seconds")
-        print("HNSW min lantency for query: ", hnsw_min_latency, "seconds")
-        print("HNSW max lantency for query: ", hnsw_max_latency, "seconds")
+        logging.info("HNSW query per seconds: ", num_queries/hnsw_total_time)
+        logging.info("HNSW average lantency per query: ", hnsw_total_time/num_queries, "seconds")
+        logging.info("HNSW min lantency for query: ", hnsw_min_latency, "seconds")
+        logging.info("HNSW max lantency for query: ", hnsw_max_latency, "seconds")
 
 def run_benchmark(dataset_name, ef_construction, M, ef_values, k=1):
 
-    print("\nRunning benchmark for:", dataset_name)
+    logging.info("\nRunning benchmark for:", dataset_name)
     dataset = read_dataset(dataset_name)
     X_train = np.array(dataset['train'])
     distance = dataset.attrs['distance']
     dimension = int(dataset.attrs['dimension']) if 'dimension' in dataset.attrs else len(X_train[0])
-    print('got a train set of size (%d * %d)' % (X_train.shape[0], dimension))
-    print('metric is: %s' % distance)
+    logging.info('got a train set of size (%d * %d)' % (X_train.shape[0], dimension))
+    logging.info('metric is: %s' % distance)
 
     for vector_count in [800000, 900000, 1000000]:
-        print("benchmark for vector count:", vector_count)
+        logging.info("benchmark for vector count:", vector_count)
         measure_all(dataset, X_train, vector_count, ef_construction = ef_construction, M=M, ef_values=ef_values, num_queries=5, k=20)
 
 def run():
