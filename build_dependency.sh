df1e976929430b82d5eba77e5bcdac84c14797e5
jinjiabao.jjb <jinjiabao.jjb@antgroup.com>
2023-08-28 21:05:17 +0800

    modify build

--- build_dependency.sh
+++ build_dependency.sh
@@ -12,6 +12,6 @@ cmake \
 make
 popd
 
-# yum install -y libaio-devel yum-utils gperftools
-# yum-config-manager --add-repo https://yum.repos.intel.com/mkl/setup/intel-mkl.repo
-# yum install -y intel-mkl
+yum install -y libaio-devel yum-utils gperftools
+yum-config-manager --add-repo https://yum.repos.intel.com/mkl/setup/intel-mkl.repo
+yum install -y intel-mkl
