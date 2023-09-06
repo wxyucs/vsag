27c56401a227aa425032841e9ecb932d02f0940b
wxy407827 <wxy407827@antgroup.com>
2023-09-06 02:29:54 +0000

    update README

--- scripts/deps/install_deps_centos.sh
+++ scripts/deps/install_deps_centos.sh
@@ -1,1 +1,1 @@
-yum install -y gfortran libaio libaio-devel
+yum install -y gfortran libaio libaio-devel python3-devel
--- scripts/deps/install_deps_ubuntu.sh
+++ scripts/deps/install_deps_ubuntu.sh
@@ -1,1 +1,1 @@
-sudo apt install -y gfortran libaio-dev
+sudo apt install -y gfortran libaio-dev python3-dev
