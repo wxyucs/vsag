23f05aae309edca9aec3cef4a5b193a638671eaa
wxy407827 <wxy407827@antgroup.com>
2023-09-07 05:21:03 +0000

    refactor Catch2 compilation

---
+++ extern/catch2/catch2.cmake
@@ -0,0 +1,8 @@
+Include(FetchContent)
+FetchContent_Declare(
+  Catch2
+  URL      http://tbase.cn-hangzhou.alipay.aliyun-inc.com/vsag%2Fthirdparty%2FCatch2-3.4.0.tar.gz
+  URL_HASH MD5=2c802a4938ed842e2942c60d1d231bb7
+)
+
+FetchContent_MakeAvailable(Catch2)
