10c001ce26d904fcd27f4cc7aaa785edb1ede06b
Zhitao Shen <zhitao.szt@antgroup.com>
2023-09-08 18:04:28 +0800

    implement diskann

---
+++ src/index/diskann.cpp
@@ -0,0 +1,5 @@
+//
+// Created by root on 9/6/23.
+//
+
+#include "diskann.h"
---
+++ src/index/diskann.h
@@ -0,0 +1,14 @@
+//
+// Created by root on 9/6/23.
+//
+
+#ifndef VSAG_DISKANN_H
+#define VSAG_DISKANN_H
+
+
+class diskann {
+
+};
+
+
+#endif //VSAG_DISKANN_H
