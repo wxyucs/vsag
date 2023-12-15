a16b3eabd3ba0fed634185aba6e1482f466dbda7
祥予 <wxy407827@antgroup.com>
2023-12-15 14:50:47 +0800

    PullRequest: 104 check supported instructions during initialization
    Merge branch 'check_instructions_at_init of git@code.alipay.com:octopus/vsag.git into master
    
    https://code.alipay.com/octopus/vsag/pull_requests/104
    
    
    Signed-off-by: 贾玮 <jw94525@antgroup.com>
    Signed-off-by: 余羡 <jinjiabao.jjb@antgroup.com>
    
    
    * check supported instructions during initialization
    
    * update
    
    * update

--- src/vsag.cpp
+++ src/vsag.cpp
@@ -1,5 +1,10 @@
 #include "vsag/vsag.h"
 
+#include <cpuinfo.h>
+#include <spdlog/spdlog.h>
+
+#include <sstream>
+
 #include "version.h"
 
 namespace vsag {
@@ -8,5 +13,29 @@ std::string
 version() {
     return VSAG_VERSION;
 }
+
+bool
+init() {
+    cpuinfo_initialize();
+    std::stringstream ss;
+
+    ss << std::boolalpha;
+    ss << "====vsag start init====";
+    ss << "\nrunning on " << cpuinfo_get_package(0)->name;
+    ss << "\ncores count: " << cpuinfo_get_cores_count();
+    ss << "\nsse: " << cpuinfo_has_x86_sse();
+    ss << "\navx: " << cpuinfo_has_x86_avx();
+    ss << "\navx2: " << cpuinfo_has_x86_avx2();
+    ss << "\navx512f: " << cpuinfo_has_x86_avx512f();
+    ss << "\navx512dq: " << cpuinfo_has_x86_avx512dq();
+    ss << "\navx512bw: " << cpuinfo_has_x86_avx512bw();
+    ss << "\navx512vl: " << cpuinfo_has_x86_avx512vl();
+    ss << "\n====vsag init done====";
+    spdlog::info(ss.str());
+
+    return true;
+}
+
+static bool _init = init();
 
 }  // namespace vsag
