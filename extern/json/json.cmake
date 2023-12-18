731269f2ae5dd7ea96f44ae4a932b5e5be282988
余羡 <jinjiabao.jjb@antgroup.com>
2023-12-18 10:19:10 +0800

    PullRequest: 105 Remove json from the codebase and switch to installation at build time.
    Merge branch rm-json of git@code.alipay.com:octopus/vsag.git into master
    https://code.alipay.com/octopus/vsag/pull_requests/105
    
    Signed-off-by: 贾玮 <jw94525@antgroup.com>
    Signed-off-by: 祥予 <wxy407827@antgroup.com>
    
    
    * Remove pybind11 from the codebase and switch to installation at build time.
    
    * Remove json from the codebase and switch to installation at build time.

---
+++ extern/json/json.cmake
@@ -0,0 +1,13 @@
+
+
+include(FetchContent)
+
+FetchContent_Declare(
+    nlohmann_json
+    URL http://tbase.cn-hangzhou.alipay.aliyun-inc.com/vsag%2Fthirdparty%2Fnlohmann_json.tar.gz?OSSAccessKeyId=LTAILFuN8FJQZ4Eu&Expires=4856232163&Signature=UPdZn3PKzrJ5rH829vw6quQc6NQ%3D
+    URL_HASH MD5=b61b15f49a1ea5c70b81696b85b1c89b
+)
+
+FetchContent_MakeAvailable(nlohmann_json)
+
+include_directories(${nlohmann_json_SOURCE_DIR}/single_include)
