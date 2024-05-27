14c1dc2044309f101161852dba62cae70531511d
祥予 <wxy407827@antgroup.com>
2024-05-27 23:44:12 +0800

    PullRequest: 181 add example of setting custom memory allocator
    Merge branch 'custom_memory_allocator_example of git@code.alipay.com:octopus/vsag.git into master
    
    https://code.alipay.com/octopus/vsag/pull_requests/181
    
    
    Signed-off-by: 贾玮 <jw94525@antgroup.com>
    Signed-off-by: 余羡 <jinjiabao.jjb@antgroup.com>
    
    
    * add example of setting custom memory allocator
    * update

---
+++ examples/cpp/custom_memory_allocator.cpp
@@ -0,0 +1,53 @@
+
+#include "vsag/vsag.h"
+
+class ExampleAllocator : public vsag::Allocator {
+public:
+    std::string
+    Name() override {
+        return "myallocator";
+    }
+
+    void*
+    Allocate(size_t size) override {
+        std::cout << Name() << ": allocate " << size << " bytes" << std::endl;
+        return malloc(size);
+    }
+
+    void
+    Deallocate(void* p) override {
+        std::cout << Name() << ": deallocate " << p << std::endl;
+        return free(p);
+    }
+
+    void*
+    Reallocate(void* p, size_t size) override {
+        std::cout << Name() << ": reallocate " << p << " with " << size << " size" << std::endl;
+        return realloc(p, size);
+    }
+};
+
+int
+main() {
+    // set allocator before creating any index
+    vsag::Options::Instance().set_allocator(std::make_unique<ExampleAllocator>());
+
+    auto paramesters = R"(
+    {
+        "dtype": "float32",
+        "metric_type": "l2",
+        "dim": 4,
+        "hnsw": {
+            "max_degree": 16,
+            "ef_construction": 100
+        }
+    }
+    )";
+    std::cout << "create index" << std::endl;
+    auto index = vsag::Factory::CreateIndex("hnsw", paramesters);
+
+    std::cout << "delete index" << std::endl;
+    index = nullptr;
+
+    return 0;
+}
